564176c7c1f880d1af4df6258fd1326a08772935
Tijuana22 <tijuanahunt39@gmail.com>
2023-01-20 12:00:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// // What is a Single Page Application?
+
+// A Single Page Application is a web application or website that interacts with the web browser by 
+// dynamically rewriting the current web page with new data from the web server, instead of the default 
+// method of the browser loading entire new pages.
+// This means that the URL of your website will not change completely (page will not reload), instead it will keep 
+// getting content and rewriting the DOM with it instead of loading a new page.
+// The goal is faster transitions that make the website feel more like a native app
+
+// //What is REACT?
+
+// A javascript library for building interfaces
+// React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. 
+// Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. 
+// The main concept of React. js is virtual DOM
+
+
+// //What is JSX?
+
+// JavaScript XML. JSX allows us to write HTML in React. JSX makes it easier to write and add HTML in React.
+// JS is standard javascript, JSX is an HTML-like syntax that you can use with React to (theoretically) make it 
+// easier and more intuitive to create React components. As the docs say, the only purpose is to make it easier to 
+// create React components... there's not much else there.
+
+// //What is Babel?
+
+// Babel is a toolchain that is mainly used to convert ECMAScript 2015+ code into a backwards compatible version of JavaScript
+//  in current and older browsers or environments. Here are the main things Babel can do for you:
+
+// Transform syntax
+
+// Polyfill features that are missing in your target environment (through a third-party polyfill such as core-js)
+// Source code transformations (codemods)
+// And more!
+
+// //What is Webpack?
+
+//  A tool that lets you bundle your JavaScript applications (supporting both ESM and CommonJS), and it can be extended 
+// to support many different assets such as images, fonts and stylesheets.
+// Like create-React-app, React Webpack is also a command-line tool used to create a bundle of assets (files and code). It doesn't 
+// run on the browser or the server. It takes all the JS files and other assets, transforming them into one large file.
+
+
+// //What is Create React App?
+
+// A tool to create single-page React applications that is officially supported by the React team. The script generates the required 
+// files and folders to start the React application and run it on the browser.
+//  It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, 
+// and optimizes your app for production.
